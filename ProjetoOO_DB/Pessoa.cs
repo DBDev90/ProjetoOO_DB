564a1d86528e565abf596008aaa69afa4fd9e2a7
DBDev90 <bordinassidouglas@gmail.com>
2023-03-08 10:22:53 -0300

    Implementação da classe Pessoa com seus métodos Get e Set, bem como instanciado no Programs

---
+++ ProjetoOO_DB/Pessoa.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoOO_DB
+{
+    public class Pessoa
+    {
+        private int Id;
+        private string Nome;
+        private string Telefone;
+
+        public Pessoa(int id, string nome, string telefone)
+        {
+            Id = id;
+            Nome = nome;
+            Telefone = telefone;
+        }
+
+        public int getId()
+        {
+            return Id;
+        }
+
+        public string getNome()
+        {
+            return Nome;
+        }
+
+        public string getTelefone()
+        {
+            return Telefone;
+        }
+
+        public void setId(int id)
+        {
+            Id = id;
+        }
+        public void setNome(string nome)
+        {
+            Nome = nome;
+        }
+        public void setTelefone(string telefone)
+        {
+            Telefone = telefone;
+        } 
+    }
+}
--- ProjetoOO_DB/Program.cs
+++ ProjetoOO_DB/Program.cs
@@ -4,6 +4,9 @@
 using ProjetoOO_DB;
 
 Produto produto1 = new Produto(1, "Caneta", 1.50M);
-Console.WriteLine(produto1.getDescricao() + "" + produto1.getValor());
+Console.WriteLine(produto1.getDescricao() + " " + produto1.getValor());
+
+Pessoa pessoa1 = new Pessoa(1, "Douglas", "16999999999");
+Console.WriteLine(pessoa1.getNome() + " " + pessoa1.getTelefone());
 
 
